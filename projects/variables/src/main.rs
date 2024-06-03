1dea30725b4529fb1e54dec63591e5e34e8d8893
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-06-04 04:52:05 +0900

    ADD: kusomize

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,12 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     let x = 5;
     println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}")
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x is: {x}");
+    }
+    println!("The value of x is: {x}");
 }
