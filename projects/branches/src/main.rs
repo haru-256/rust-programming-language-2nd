e8882e2c530211d4c1053bfdd2b0faaf21b60a05
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-06-06 23:04:15 +0900

    ADD: add fibonacci number

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -54,4 +54,10 @@ fn main() {
         println!("{number}!")
     }
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
