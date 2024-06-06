6e422060df064441de3418ff9b2108fa5d0a6e30
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-06-06 22:52:03 +0900

    ADD: branches

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    let x = another_function(3, 'h');
+    println!("The value of x is: {}", x);
+
+    let y = {
+        let x = 6;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(value: i32, unit_label: char) -> i32 {
+    println!("Another function. x is {value}{unit_label}");
+    return value + 1;
+    // value + 1
 }
